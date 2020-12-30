1bbfe67fc2e85af06ec93331a24a42505068ebe8
Vagusko <p.kolinko95@gmail.com>
2020-12-30 12:28:06 +0100

    add slider

--- FT-1-Generator/view/appView.py
+++ FT-1-Generator/view/appView.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QLineEdit, QLabel, QGridLayout, QPushButton, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QLineEdit, QLabel, QGridLayout, QPushButton, QMainWindow, QVBoxLayout, QWidget, QSlider
 
 
 class View(QMainWindow):
@@ -17,6 +19,7 @@ class View(QMainWindow):
         self._centralWidget.setLayout(self.generalLayout)
         # Create the display and the buttons
         self._createDirectories()
+        self._createSlide()
         self._createButtons()
 
     # Snip
@@ -26,30 +29,31 @@ class View(QMainWindow):
         buttonsLayout = QGridLayout()
         # Button text | position on the QGridLayout
         directories = {
-            '<h3>Input file:</h3>': (0, 1),
-            'inputFileDir': (0, 2),
-            '<h3>Output file:</h3>': (0, 3),
-            'outputFileDir': (0, 4),
+            '<h3>Input file:</h3>': (0, 0),
+            'inputFileDir': (0, 1),
+            '<h3>Output file:</h3>': (0, 2),
+            'outputFileDir': (0, 3),
         }
         # Create the buttons and add them to the grid layout
         for btnText, pos in directories.items():
             if btnText == 'inputFileDir' or btnText == 'outputFileDir':
                 self.directories[btnText] = QLineEdit()
+                self.directories[btnText].setFixedSize(180, 30)
             else:
                 self.directories[btnText] = QLabel()
                 self.directories[btnText].setText(btnText)
-            self.directories[btnText].setFixedSize(100, 30)
+                self.directories[btnText].setFixedSize(70, 30)
+
             buttonsLayout.addWidget(self.directories[btnText], pos[0], pos[1])
         # Add buttonsLayout to the general layout
         self.generalLayout.addLayout(buttonsLayout)  # Snip
 
     def _createButtons(self):
-        """Create the buttons."""
         self.buttons = {}
         buttonsLayout = QGridLayout()
         # Button text | position on the QGridLayout
         buttons = {
-            'Generate': (0, 0),
+            'Generate': (0, 1),
         }
         # Create the buttons and add them to the grid layout
         for btnText, pos in buttons.items():
@@ -59,7 +63,35 @@ class View(QMainWindow):
         # Add buttonsLayout to the general layout
         self.generalLayout.addLayout(buttonsLayout)
 
-    # Snip
+    def _createSlide(self):
+        self.maskSize = 0
+        self.maskSizeSlider = 0
+        slideLayout = QGridLayout()
+
+        # Label
+        self.slideLabel = QLabel()
+        self.slideLabel.setText("<h3>Mask size:</h3>")
+        self.slideLabel.setFixedSize(680, 30)
+        slideLayout.addWidget(self.slideLabel, 0, 0)
+
+        # Slider
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(10)
+        self.slider.setValue(5)
+        self.slider.setTickPosition(QSlider.TicksBelow)
+        self.slider.setTickInterval(1)
+        self.slider.setFixedSize(310, 30)
+        slideLayout.addWidget(self.slider, 1, 0)
+
+
+        # Add buttonsLayout to the general layout
+        self.generalLayout.addLayout(slideLayout)
+
+    def valuechange(self):
+        size = self.sl.value()
+        self.l1.setFont(QFont("Arial", size))
+
     def setDisplayText(self, text):
         """Set display's text."""
         self.display.setText(text)
