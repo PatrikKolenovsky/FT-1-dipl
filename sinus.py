4541a0f31d3bc8f1c66d4ed160291702584167c0
Vagusko <p.kolenovsky95@gmail.com>
2021-04-28 15:02:04 +0200

    - sinus example ploting

--- sinus.py
+++ sinus.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 x = np.arange(0, 2 * np.pi, 0.1)  # start,stop,step
 y = np.sin(x)
 
+points = []
+plt.xlabel('x values from 0 to 2pi')  # string must be enclosed with quotes '  '
+plt.ylabel('sin(x)')
+plt.title('Plot of sin from 0 to 2pi')
+# 1/6 pi a 2/6 pi
+plt.plot(points, 'ko', color="red", markersize=5, label="1/6 Pi")
+plt.plot(points, 'ko', color="blue", markersize=5, label="2/6 Pi")
+plt.plot(x, y, label="Sine")
+plt.legend()
 
-plt.plot(x,y)
 plt.show()
